b2f54bd3bb07a882368b6612b7ffb21092b307cc
LisandraN08 <lnicoline@student.ciputra.ac.id>
2023-10-20 08:19:12 +0700

    Initial Commit

---
+++ CodingFlight_W06/CodingFlight_W06App.swift
@@ -0,0 +1,17 @@
+//
+//  CodingFlight_W06App.swift
+//  CodingFlight_W06
+//
+//  Created by MacBook Pro on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CodingFlight_W06App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
